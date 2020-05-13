a66ecf91af2ec01252e9f45b02e86e1a64e3b525
RatnaRajpal <54902237+RatnaRajpal@users.noreply.github.com>
2020-05-13 15:05:55 +0530

    Update App.js

--- WebApp/dashboard/src/App.js
+++ WebApp/dashboard/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import CustomerDetails from './CustomerDetails'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      React Boiler Plate
+    <CustomerDetails />
     </div>
   );
 }
